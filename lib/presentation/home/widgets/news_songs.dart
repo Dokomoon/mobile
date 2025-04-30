f5f7a1f1da8881ee084dea11b558dc86005e62a2
Odbayar70 <b221940170@erdenetis.edu.mn>
2025-04-30 14:03:24 +0800

    n

--- lib/presentation/home/widgets/news_songs.dart
+++ lib/presentation/home/widgets/news_songs.dart
@@ -1,31 +1,67 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotipyt/core/configs/constants/app_urls.dart';
+import 'package:spotipyt/domain/entities/song/song.dart';
+import 'package:spotipyt/presentation/home/bloc/news_songs_cubit.dart';
+import 'package:spotipyt/presentation/home/bloc/news_songs_state.dart';
 
 class NewsSongs extends StatelessWidget {
   const NewsSongs({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("News Songs")),
-      body: Column(
-        children: [
-          // 🔽 Put the debug button here temporarily
-          ElevatedButton(
-            onPressed: () async {
-              var snap =
-                  await FirebaseFirestore.instance.collection('Songs').get();
-              print("Songs found: ${snap.docs.length}");
-              if (snap.docs.isNotEmpty) {
-                print("First song: ${snap.docs.first.data()}");
-              }
-            },
-            child: Text("Test Firestore"),
-          ),
-
-          Expanded(child: NewsSongs()), // Your original widget
-        ],
+    return BlocProvider(
+      create: (_) => NewsSongsCubit()..getNewsSongs(),
+      child: SizedBox(
+        height: 200,
+        child: BlocBuilder<NewsSongsCubit, NewsSongsState>(
+          builder: (context, state) {
+            if (state is NewsSongsLoading) {
+              return Container(
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (state is NewsSongsLoaded) {
+              return _songs(state.songs);
+            }
+            return Container();
+          },
+        ),
       ),
+    );
+  }
+
+  Widget _songs(List<SongEntity> songs) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return SizedBox(
+          width: 160,
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                        '${AppURLs.firestorage}${songs[index].artist} - ${songs[index].title}.jpg${AppURLs.mediaAlt}',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(songs[index].title),
+            ],
+          ),
+        );
+      },
+      separatorBuilder: (context, index) => SizedBox(width: 14),
+      itemCount: songs.length,
     );
   }
 }
